b7244a7d0abb062b113bc492c85bbd00b96b6206
Alexia Lewis <lewisalexia208@gmail.com>
2023-07-10 15:46:29 -0500

    acquire function created and tested locally

---
+++ wrangle.py
@@ -0,0 +1,51 @@
+#standard imports
+import pandas as pd 
+
+#get/create files
+import os
+from google.oauth2 import service_account
+import pandas_gbq
+
+#splitting
+from sklearn.model_selection import train_test_split
+
+#scaling
+from sklearn.preprocessing import MinMaxScaler
+
+#vizzes
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+print(f'imports loaded successfully, awaiting commands...')
+
+#------------------------------------------------------------- ACQUIRE -------------------------------------------------------------
+    
+def check_file_exists_gbq(fn):
+    '''
+    check if file exists in my local directory, if not, pull from google big query db
+    using service key token, return dataframe and save to a csv in the local directory
+    '''
+    #credentials
+    credentials = service_account.Credentials.from_service_account_file(
+    'service_key.json',)
+    
+    #if/else file exists in local directory
+    if os.path.isfile(fn):
+        print('csv file found and loaded')
+        return pd.read_csv(fn, index_col=0)
+    else: 
+        print('creating df and exporting csv')
+        df = pandas_gbq.read_gbq("select * from cfpb_complaints.complaint_database",dialect='standard',project_id="my-ds-projects",use_bqstorage_api=True,credentials=credentials)
+        df.to_csv(fn)
+        return df
+    
+#------------------------------------------------------------- PREPARE -------------------------------------------------------------
+
+
+
+#------------------------------------------------------------- VIZZES -------------------------------------------------------------
+
