34d6c2a3ab2efc47b7533010addec3ee090c6044
Tyler Kephart <tkephart96@gmail.com>
2023-07-20 09:40:16 -0500

    2nd iter models

--- model.py
+++ model.py
@@ -11,17 +11,17 @@
 import pandas as pd
 import numpy as np
 import itertools
-from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB, ComplementNB, CategoricalNB
+from sklearn.svm import LinearSVC
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
-import nltk
-import nltk.sentiment
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics import recall_score
 
 #feature importance
 from sklearn.feature_selection import SelectKBest, chi2, mutual_info_classif
@@ -37,6 +37,7 @@ from sklearn.feature_selection import SelectKBest, chi2, mutual_info_classif
 """
 
 #-------------------------------------------------------------VECTORIZERS -------------------------------------------------------------
+
 
 def make_cv(Xtr,Xv,Xt):
     """
@@ -125,6 +126,7 @@ def make_mbt_cv(Xtr,Xv,Xt):
     Xt_cv = pd.DataFrame(Xt_bow_cv.todense(),columns=cv.get_feature_names_out(),index=Xt.index)
     return Xtr_cv,Xv_cv,Xt_cv
 
+
 def make_mbt_tfidf(Xtr,Xv,Xt):
     """
     The function `make_tfidf` takes in three sets of data (train, validation, and test) and applies the
@@ -152,6 +154,7 @@ def make_mbt_tfidf(Xtr,Xv,Xt):
     Xv_tfidf = pd.DataFrame(Xv_bow_tfidf.todense(),columns=tfidf.get_feature_names_out(),index=Xv.index)
     Xt_tfidf = pd.DataFrame(Xt_bow_tfidf.todense(),columns=tfidf.get_feature_names_out(),index=Xt.index)
     return Xtr_tfidf,Xv_tfidf,Xt_tfidf
+
 
 def make_mbt_tfidf2(Xtr,Xv,Xt):
     """
@@ -181,7 +184,21 @@ def make_mbt_tfidf2(Xtr,Xv,Xt):
     Xt_tfidf = pd.DataFrame(Xt_bow_tfidf.todense(),columns=tfidf.get_feature_names_out(),index=Xt.index)
     return Xtr_tfidf,Xv_tfidf,Xt_tfidf
 
+
 def process_vector_merge(X_train,X_val,X_test):
+    """
+    The function `process_vector_merge` takes in three dataframes (`X_train`, `X_val`, `X_test`),
+    performs some transformations on the data, and returns the merged dataframes.
+    
+    :param X_train: X_train is a pandas DataFrame containing the training data. It should have columns
+    representing different features, with the target variable in the first column
+    :param X_val: X_val is a pandas DataFrame containing the validation data. It has multiple columns,
+    with the 'lemon' column being used for creating tf-idf vectors. The remaining columns are used for
+    encoding
+    :param X_test: X_test is a DataFrame containing the test data. It should have the same columns as
+    X_train and X_val
+    :return: three dataframes: X_train_tfe, X_val_tfe, and X_test_tfe.
+    """
     X_train_tf, X_val_tf, X_test_tf = make_mbt_tfidf(X_train[['lemon']], X_val[['lemon']], X_test[['lemon']])
     
     encoded_train = X_train.iloc[:, 1:]
@@ -195,7 +212,22 @@ def process_vector_merge(X_train,X_val,X_test):
     # Visualization of train data
     return X_train_tfe, X_val_tfe, X_test_tfe
 
+
 def process_vector_merge2(X_train,X_val,X_test):
+    """
+    The function `process_vector_merge2` takes three input dataframes (`X_train`, `X_val`, `X_test`),
+    performs some transformations on the data, and returns three modified dataframes (`X_train_tfe`,
+    `X_val_tfe`, `X_test_tfe`).
+    
+    :param X_train: X_train is a pandas DataFrame containing the training data. It should have columns
+    representing different features or variables
+    :param X_val: X_val is a pandas DataFrame containing the validation data. It has multiple columns,
+    with the 'lemon' column being used for creating tf-idf vectors. The remaining columns are used for
+    encoding
+    :param X_test: X_test is a DataFrame containing the test data. It should have the same columns as
+    X_train and X_val
+    :return: three dataframes: X_train_tfe, X_val_tfe, and X_test_tfe.
+    """
     X_train_tf, X_val_tf, X_test_tf = make_mbt_tfidf2(X_train[['lemon']], X_val[['lemon']], X_test[['lemon']])
     
     encoded_train = X_train.iloc[:, 1:]
@@ -220,14 +252,6 @@ def baseline(y_train):
     :return: The percentage of the majority class in the y_train dataset, rounded to 4 decimal places.
     """
     print(f'Baseline Accuracy: {round(y_train.value_counts(normalize=True)[0],4)*100}%')
-
-
-def baseline2():
-    '''
-    gets the baseline for second iteration notebook
-    '''
-    baseline_accuracy = round(('no_relief' == df.response).mean(),4) * 100
-    print(f'Baseline Accuracy is {baseline_accuracy}%')
 
 
 def process_data_modeling(train, validate, test):
@@ -276,13 +300,13 @@ def process_data_modeling(train, validate, test):
     small_test.reset_index(drop=True, inplace=True)
 
     X_train = encode(small_train)
-    X_train = X_train.drop(columns=['date_received','company_response_to_consumer','clean','state','company_name','tags','product_bins'])
+    X_train = X_train.drop(columns=['date_received','company_response_to_consumer','state','company_name','tags','product_bins'])
     y_train = small_train['company_response_to_consumer']
     X_val = encode(small_val)
-    X_val = X_val.drop(columns=['date_received','company_response_to_consumer','clean','state','company_name','tags','product_bins'])
+    X_val = X_val.drop(columns=['date_received','company_response_to_consumer','state','company_name','tags','product_bins'])
     y_val = small_val['company_response_to_consumer']
     X_test = encode(small_test)
-    X_test = X_test.drop(columns=['date_received','company_response_to_consumer','clean','state','company_name','tags','product_bins'])
+    X_test = X_test.drop(columns=['date_received','company_response_to_consumer','state','company_name','tags','product_bins'])
     y_test = small_test['company_response_to_consumer']
 
     return X_train, y_train, X_val, y_val, X_test, y_test
@@ -331,13 +355,13 @@ def process_data_modeling2(train, validate, test):
     small_test.reset_index(drop=True, inplace=True)
 
     X_train = encode(small_train)
-    X_train = X_train.drop(columns=['date_received','clean','state','company_name','tags','product_bins', 'response'])
+    X_train = X_train.drop(columns=['date_received','state','company_name','tags','product_bins', 'response'])
     y_train = small_train['response']
     X_val = encode(small_val)
-    X_val = X_val.drop(columns=['date_received','clean','state','company_name','tags','product_bins', 'response'])
+    X_val = X_val.drop(columns=['date_received','state','company_name','tags','product_bins', 'response'])
     y_val = small_val['response']
     X_test = encode(small_test)
-    X_test = X_test.drop(columns=['date_received', 'clean','state','company_name','tags','product_bins', 'response'])
+    X_test = X_test.drop(columns=['date_received','state','company_name','tags','product_bins', 'response'])
     y_test = small_test['response']
 
     return X_train, y_train, X_val, y_val, X_test, y_test
@@ -477,6 +501,115 @@ def test_tree(Xtr,ytr,Xt,yt):
     print(f'Test Accuracy:     {round(yt_acc,4)*100}%')
 
 
+def tree_multigram_model2(Xtr,ytr,Xv,yv):
+    """
+    The function `tree_multigram_model` trains a decision tree classifier using monograms, bigrams, and
+    trigrams as features and prints the train and validation accuracies.
+    
+    :param Xtr: Xtr is the training data features. It is a matrix or array-like object that contains the
+    features of the training data. Each row represents a sample and each column represents a feature
+    :param ytr: The parameter `ytr` represents the target variable for the training set. It is the set
+    of labels or classes that the model will try to predict based on the features in `Xtr`
+    :param Xv: Xv is the validation set features. It is a matrix or array-like object that contains the
+    features of the validation set. Each row represents a sample, and each column represents a feature
+    :param yv: The parameter "yv" represents the target variable for the validation set. It is the true
+    labels or classes for the validation set samples
+    """
+    # unique features and tfidf
+    # decision tree
+    tree = DecisionTreeClassifier(criterion='gini',max_depth=9,min_samples_leaf=11,max_features='sqrt',random_state=123)
+    tree.fit(Xtr,ytr)
+    # predictions
+    y_pred_train = tree.predict(Xtr)
+    y_pred_val = tree.predict(Xv)
+    # recall score
+    tr_rec = recall_score(ytr, y_pred_train, pos_label='no_relief')
+    v_rec = recall_score(yv, y_pred_val, pos_label='no_relief')
+    # accuracies
+    ytr_acc = tree.score(Xtr,ytr)
+    yv_acc = tree.score(Xv,yv)
+    # print results
+    print('Decision Tree Unigrams, Bigrams, Trigrams')
+    print(f'Train Accuracy:      {round(ytr_acc*100,4)}%')
+    print(f'Validation Accuracy: {round(yv_acc*100,4)}%')
+    print(f'Train Recall:        {round(tr_rec*100,4)}%')
+    print(f'Validation Recall:   {round(v_rec*100,4)}%')
+
+
+def svc_multigram_model(Xtr,ytr,Xv,yv):
+    """
+    The function `svc_multigram_model` trains a linear support vector classifier (SVC) model using
+    unigrams, bigrams, and trigrams as features, and then evaluates the model's accuracy and recall
+    scores on the training and validation datasets.
+    
+    :param Xtr: Xtr is the training data, which is a matrix or array-like object containing the features
+    for training the model. Each row represents a sample, and each column represents a feature
+    :param ytr: The parameter `ytr` represents the target variable (labels) for the training data. It is
+    a one-dimensional array or list containing the labels for each sample in the training set
+    :param Xv: Xv is the feature matrix for the validation set. It contains the input features for each
+    instance in the validation set
+    :param yv: The parameter `yv` represents the true labels of the validation set. It is a list or
+    array containing the actual labels for each instance in the validation set
+    """
+    # unique features and tfidf
+    # linear svc
+    sv = LinearSVC(penalty='l2',C=0.1,dual=False,random_state=123)
+    sv.fit(Xtr,ytr)
+    # predictions
+    y_pred_train = sv.predict(Xtr)
+    y_pred_val = sv.predict(Xv)
+    # recall score
+    tr_rec = recall_score(ytr, y_pred_train, pos_label='no_relief')
+    v_rec = recall_score(yv, y_pred_val, pos_label='no_relief')
+    # accuracies
+    ytr_acc = sv.score(Xtr,ytr)
+    yv_acc = sv.score(Xv,yv)
+    # print results
+    print('Linear SVC Unigrams, Bigrams, Trigrams')
+    print(f'Train Accuracy:      {round(ytr_acc*100,4)}%')
+    print(f'Validation Accuracy: {round(yv_acc*100,4)}%')
+    print(f'Train Recall:        {round(tr_rec*100,4)}%')
+    print(f'Validation Recall:   {round(v_rec*100,4)}%')
+
+
+def mlp_multigram_model(Xtr,ytr,Xv,yv):
+    """
+    The function `mlp_multigram_model` trains a multi-layer perceptron model using unigrams, bigrams,
+    and trigrams as features and prints the train and validation accuracies, as well as the train and
+    validation recall scores.
+    
+    :param Xtr: Xtr is the training data features, which are the input variables used to train the MLP
+    model. It should be a matrix or array-like object with shape (n_samples, n_features), where
+    n_samples is the number of samples in the training data and n_features is the number of features or
+    input
+    :param ytr: ytr is the target variable for the training set. It contains the labels or classes
+    corresponding to the input data Xtr
+    :param Xv: Xv is the feature matrix for the validation set. It contains the input features for each
+    instance in the validation set
+    :param yv: The parameter "yv" represents the target variable for the validation set. It contains the
+    true labels for the validation data
+    """
+    # unique features and tfidf
+    # multi-layer perceptron
+    ml = MLPClassifier(activation='relu',solver='adam',random_state=123,early_stopping=True)
+    ml.fit(Xtr,ytr)
+    # predictions
+    y_pred_train = ml.predict(Xtr)
+    y_pred_val = ml.predict(Xv)
+    # recall score
+    tr_rec = recall_score(ytr, y_pred_train, pos_label='no_relief')
+    v_rec = recall_score(yv, y_pred_val, pos_label='no_relief')
+    # accuracies
+    ytr_acc = ml.score(Xtr,ytr)
+    yv_acc = ml.score(Xv,yv)
+    # print results
+    print('Multi-Layer Perceptron Unigrams, Bigrams, Trigrams')
+    print(f'Train Accuracy:      {round(ytr_acc*100,4)}%')
+    print(f'Validation Accuracy: {round(yv_acc*100,4)}%')
+    print(f'Train Recall:        {round(tr_rec*100,4)}%')
+    print(f'Validation Recall:   {round(v_rec*100,4)}%')
+
+
 #------------------------------------------------------------- LOOPING MODELs -------------------------------------------------------------
 
 
@@ -513,7 +646,26 @@ def tree_models(Xtr,ytr,Xv,yv):
         metrics.append(output)
     return pd.DataFrame(metrics)
 
+
 def tree_models(Xtr,ytr,Xv,yv):
+    """
+    The function `tree_models` trains decision tree models with different combinations of
+    hyperparameters and returns a DataFrame containing the training and validation accuracies for each
+    model.
+    
+    :param Xtr: Xtr is the training data for the features (independent variables). It is a matrix or
+    dataframe with shape (n_samples, n_features), where n_samples is the number of samples in the
+    training set and n_features is the number of features
+    :param ytr: The variable `ytr` represents the target variable (dependent variable) for the training
+    set. It contains the actual values that we are trying to predict or classify
+    :param Xv: Xv is the validation set features. It is a matrix or dataframe containing the input
+    features for the validation set
+    :param yv: The variable `yv` represents the target variable for the validation set. It is the true
+    values of the target variable for the validation set
+    :return: a pandas DataFrame containing the metrics for different decision tree models. The metrics
+    include the model name, the parameters used for each model, the training accuracy, and the
+    validation accuracy.
+    """
     metrics = []
     # cycle through depth,leaf,class_weight for dec tree
     for d,l in itertools.product(range(5,11),range(5,11)):
@@ -532,6 +684,7 @@ def tree_models(Xtr,ytr,Xv,yv):
             }
         metrics.append(output)
     return pd.DataFrame(metrics)
+
 
 def knn_models(Xtr,ytr,Xv,yv):
     """
@@ -565,7 +718,24 @@ def knn_models(Xtr,ytr,Xv,yv):
         metrics.append(output)
     return pd.DataFrame(metrics)
 
+
 def log_models(Xtr,ytr,Xv,yv):
+    """
+    The function `log_models` performs logistic regression on different values of C and returns a
+    DataFrame with the model parameters and accuracy scores for training and validation data.
+    
+    :param Xtr: Xtr is the training data features. It is a matrix or dataframe containing the input
+    features for training the logistic regression model
+    :param ytr: The variable `ytr` represents the target variable for the training set. It is the set of
+    labels or classes that the model will try to predict based on the features in the training set `Xtr`
+    :param Xv: Xv is the validation set features. It is a matrix or dataframe containing the input
+    features for the validation set
+    :param yv: The variable `yv` represents the target variable for the validation set. It is the true
+    labels for the validation set
+    :return: a pandas DataFrame containing the metrics for each logistic regression model trained with
+    different values of C. The DataFrame includes the model name, the model parameters, training
+    accuracy, and validation accuracy for each model.
+    """
     metrics = []
     # cycle through C for log reg
     for c in [.01,.1,1,10,100,1000]:
@@ -753,8 +923,6 @@ def select_kbest(X, y, k=2, scoring=chi2):
     return kbest_results.sort_values(by=['feature_score'], ascending=False).head(k)
 
 
-
-
 def multi_nb_hyperparam_search(X_train, y_train, X_val, y_val, hyperparams):
     """
     Performs a hyperparameter search for Multinomial Naive Bayes models and returns the results.
@@ -799,7 +967,24 @@ def multi_nb_hyperparam_search(X_train, y_train, X_val, y_val, hyperparams):
     
     return df_results
 
+
 def forest_models(Xtr,ytr,Xv,yv):
+    """
+    The function `forest_models` trains and evaluates random forest models with different combinations
+    of hyperparameters and returns a DataFrame containing the model's parameters and accuracy scores on
+    the training and validation sets.
+    
+    :param Xtr: Xtr is the training data features. It is a matrix or dataframe containing the input
+    features for training the random forest model
+    :param ytr: The variable `ytr` represents the target variable (or the dependent variable) for the
+    training set. It contains the actual values that we are trying to predict or classify
+    :param Xv: Xv is the validation set features. It is a matrix or dataframe containing the input
+    features for the validation set
+    :param yv: The variable "yv" represents the target variable for the validation set. It is the true
+    values of the target variable for the validation set
+    :return: a pandas DataFrame containing the metrics for different random forest models. The DataFrame
+    has columns for the model name, model parameters, training accuracy, and validation accuracy.
+    """
     metrics = []
     # cycle through depth,leaf,class_weight for random forest
     for d,l in itertools.product(range(5,11),range(5,11)):
